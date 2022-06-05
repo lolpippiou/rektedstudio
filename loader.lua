1dd85b7308b885875b4d1436fef2fab0d7a07b04
lolpippiou <50070094+lolpippiou@users.noreply.github.com>
2022-06-05 12:58:22 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -7,16 +7,4 @@ local function ontp(s)
 end
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/lolpippiou/rektedstudio/main/script.lua"))()
-ontp([[
-    local function ontp(s)
-        if syn and syn.queue_on_teleport then
-            syn.queue_on_teleport(s)
-        elseif queue_on_teleport then
-            queue_on_teleport(s)
-        end
-    end
-    
-    repeat wait() until game:IsLoaded()
-    
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/lolpippiou/rektedstudio/main/script.lua"))()
-]])
+ontp([[loadstring(game:HttpGet("https://raw.githubusercontent.com/lolpippiou/rektedstudio/main/loader.lua"))()]])
